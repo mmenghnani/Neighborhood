a05099a1523e672bc8ece67baf7d2324dc2706b0
Mohit Menghnani <m.menghnani@gmail.com>
2018-02-03 21:37:44 -0800

    Added the support for mobile and implemented the hamburger for small screen sizes

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
 var locations = [
         {
+            name : 'Palace of Fine Arts',
             lat : 37.8027066,
             lng : -122.4494161
          },
-            {
-            lat: 37.80588333008106,
-            lng: -122.42303703974015
-        },
+        /*{
+            name : 'San Francisco Fisherman Wharf',
+            lat: 37.8058875,
+            lng: -122.4252257
+        },*/
         {
+            name : 'Coit Tower',
             lat: 37.8024286,
             lng: -122.4058044
         },
         {
+            name : 'San Francisco City Hall',
             lat : 37.77990729540689,
             lng : -122.41996637269969
         },
         {
+            name : 'Golden Gate Bridge',
             lat: 37.8199924,
             lng: -122.4788889
         },
         {
+            name : 'Alcatraz Island',
             lat: 37.828125,
             lng: -122.422844
         },
         {
+            name : 'Pier 39',
             lat: 37.8098305727594,
             lng: -122.4103707075119
         },
 
-        {
+        {   name : 'California Academy of Sciences',
             lat : 37.769951624839024,
             lng : -122.46643060712651
         },
         {
+            name : 'Painted ladies',
             lat : 37.7762593,
             lng : -122.432758
         }
@@ -43,13 +51,13 @@ prevScope = null; //setting up a global state variable for left nav item and mar
 //Map Creation and initialization. The ko binding is also being called from here.
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
+        zoom: 13,
         mapTypeControlOptions: {
               style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
               position: google.maps.ControlPosition.TOP_CENTER
           },
           zoomControlOptions: {
-              position: google.maps.ControlPosition.RIGHT_CENTER
+              position: google.maps.ControlPosition.LEFT_CENTER
           },
          center : {lat : 37.8040051173 , lng: -122.436809919}
     });
@@ -68,7 +76,7 @@ var createLocationObject = function (data) {
     var self = this;
     var def = $.Deferred();
     var retVal = {
-        name: "",
+        name: data.name,
         lat: data.lat,
         lng: data.lng,
         streetName: "",
@@ -96,8 +104,7 @@ var createLocationObject = function (data) {
         this.infoWindow.setContent(this.contentString);
         prevScope = this;
         clickActions(this);
-         }.bind(retVal));
-
+        }.bind(retVal));
 
     //Autorization keys for the API
     clientID = "C2BEK4YGZW2O5TGDFVDIE3MFJWH3U1WZ3N2VJJ3BLYM4PIHE";
@@ -106,12 +113,12 @@ var createLocationObject = function (data) {
     //Creating the API url to make the call
     var foursquareURL = 'https://api.foursquare.com/v2/'+
     'venues/search?ll='+data.lat + ',' + data.lng + '&client_id=' + clientID +
-                '&client_secret=' + clientSecret + '&v=20180124';
+                '&client_secret=' + clientSecret + '&v=20180124'+'&query=' + data.name +
+          '&intent=match';
 
 //create an object(retVal) for each lat long with data returned from foursqaure api
     $.getJSON(foursquareURL).done(function(data){
             var apiResults = data.response.venues[0];
-            retVal.name = apiResults.name;
             retVal.streetName = apiResults.location.formattedAddress[0]; //the first
             retVal.city = apiResults.location.formattedAddress[1];
             retVal.phone = apiResults.contact.formattedPhone;
@@ -119,16 +126,22 @@ var createLocationObject = function (data) {
                     retVal.phone = '';
                 }
             retVal.url = apiResults.url;
-                if(!(retVal.url)){
-                    retVal.url = null;
-                }
+            if(retVal.url){ //if URL is not returned from the API call. the content string in the infoWindow won't have the URL tag
             retVal.contentString = '<div class="info-window-content"><div class="title"><b>' + retVal.name + "</b>" + //building the content for the infowindow
                 '<a href="' + retVal.url +'"> (website) </a></div>' +
                 '<div class="content">' + retVal.streetName + "</div>" +
                 '<div class="content">' + retVal.city + "</div>" +
-                '<div class="content">' + retVal.phone + "</a></div></div>";
-            retVal.contentString += '<img class="images" src= http://maps.googleapis.com/maps/api/streetview?'
-            +'size=200x100&location=' + retVal.lat + "," + retVal.lng + '>';
+                '<div class="content">' + retVal.phone + "</a></div></div>" +
+                '<img class="images" src= http://maps.googleapis.com/maps/api/streetview?size=200x100&location=' + retVal.lat + "," + retVal.lng + '>';
+            }
+            else {
+                retVal.contentString = '<div class="info-window-content"><div class="title"><b>' + retVal.name + "</b>" +
+                '</div>' +
+                '<div class="content">' + retVal.streetName + "</div>" +
+                '<div class="content">' + retVal.city + "</div>" +
+                '<div class="content">' + retVal.phone + "</a></div></div>" +
+                '<img class="images" src= http://maps.googleapis.com/maps/api/streetview?size=200x100&location=' + retVal.lat + "," + retVal.lng + '>';
+            }
             //Opening InfoWindow with a click
             retVal.infoWindow = new google.maps.InfoWindow({content: retVal.contentString});
             def.resolve(retVal);
@@ -159,6 +172,7 @@ var createLocationObject = function (data) {
     var clickActions = function (data) {
         data.infoWindow.open(map,data.marker);
         data.marker.setIcon('https://goo.gl/iDKehU');
+        map.setCenter(new google.maps.LatLng(data.lat, data.lng));//setting map center as current lat and lng.
         highlightLeftNavStyle(data);
         }
 
